07810d328e70cfb339d7f278f8f0bace9c56ae64
Matthew Stiles <mstiles92@gmail.com>
2018-04-18 17:28:12 -0400

    Simplify install and uninstall scripts

--- install.sh
+++ install.sh
@@ -1,39 +1,48 @@
 #!/usr/bin/env bash
-
 set -x
 dir=$(pwd)
 
 source "${dir}/utils.sh"
 
-install() {
-	if ask "Install symlink for .bashrc and .profile?" Y; then
-		ln -sfb -S .original ${dir}/.bashrc ${HOME}/.bashrc
-		ln -sfb -S .original ${dir}/.profile ${HOME}/.profile
-	fi
+link_file() {
+	local relPath=$1
+	local linkPath=$HOME/$relPath
+	local linkTarget=$dir/$relPath
 	
-	if ask "Install symlinks for i3 files?" Y; then
-		mkdir -p ${HOME}/.config/i3
-		ln -sfb -S .original ${dir}/.config/i3/config ${HOME}/.config/i3/config
-		ln -sfb -S .original ${dir}/.config/i3/i3blocks.conf ${HOME}/.config/i3/i3blocks.conf
-		mkdir -p ${HOME}/Scripts
-		ln -sfb -S .original ${dir}/Scripts/toggle-touchpad.sh ${HOME}/Scripts/toggle-touchpad.sh
-		ln -sfb -S .original ${dir}/Scripts/lock.sh ${HOME}/Scripts/lock.sh
-		ln -sfb -S .original ${dir}/Scripts/lock-icon.png ${HOME}/Scripts/lock-icon.png
+	if [[ $2 ]]; then
+		linkTarget=$2
 	fi
-	
-	if [ -e ${HOME}/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox ]; then
-		if ask "Install symlink to ~/.local/bin for JetBrains Toolbox?" Y; then
-			mkdir -p ${HOME}/.local/bin
-			ln -sfb -S .original ${HOME}/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox ${HOME}/.local/bin/jetbrains-toolbox
-		fi
-	fi
-	
-	if [ -e /opt/pia/run.sh ]; then
-		if ask "Install symlink to ~/.local/bin for PrivateInternetAccess?" Y; then
-			mkdir -p ${HOME}/.local/bin
-			ln -sfb -S .original /opt/pia/run.sh ${HOME}/.local/bin/pia
-		fi
-	fi
+
+	ln -sfb -S .original "$linkTarget" "$linkPath"
 }
 
-install
+if ask "Install symlink for .bashrc and .profile?" Y; then
+	link_file ".bashrc"
+	link_file ".profile"
+fi
+
+if ask "Install symlinks for i3 files?" Y; then
+	mkdir -p $HOME/.config/i3
+	mkdir -p $HOME/Scripts
+	link_file ".config/i3/config"
+	link_file ".config/i3/i3blocks.conf"
+	link_file "Scripts/toggle-touchpad.sh"
+	chmod u+x $dir/Scripts/toggle-touchpad.sh
+	link_file "Scripts/lock.sh"
+	chmod u+x $dir/Scripts/lock.sh
+	link_file "Scripts/lock-icon.png"
+fi
+
+if [ -e $HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox ]; then
+	if ask "Install symlink to ~/.local/bin for JetBrains Toolbox?" Y; then
+		mkdir -p $HOME/.local/bin
+		link_file ".local/bin/jetbrains-toolbox" "$HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox"
+	fi
+fi
+
+if [ -e /opt/pia/run.sh ]; then
+	if ask "Install symlink to ~/.local/bin for PrivateInternetAccess?" Y; then
+		mkdir -p $HOME/.local/bin
+		link_file ".local/bin/pia" "/opt/pia/run.sh"
+	fi
+fi
--- uninstall.sh
+++ uninstall.sh
@@ -5,44 +5,34 @@ dir=$(pwd)
 
 source "${dir}/utils.sh"
 
-uninstall() {
-	if [ -e ${HOME}/.bashrc.original ]; then
-		mv ${HOME}/.bashrc.original ${HOME}/.bashrc
+unlink_file() {
+	local relPath=$1
+	local linkPath=$HOME/$relPath
+	
+	local actualLinkTarget=`readlink $linkPath`
+	local expectedLinkTarget=$dir/$relPath
+	
+	if [[ $2 ]]; then
+		expectedLinkTarget=$2
+	fi
+
+	if [ -L "$linkPath" ] && [ "$actualLinkTarget" = "$expectedLinkTarget" ]; then
+		rm $linkPath
 	fi
 	
-	if [ -e ${HOME}/.profile.original ]; then
-		mv ${HOME}/.profile.original ${HOME}/.profile
-	fi
-	
-	if [ -e ${HOME}/.config/i3/config.original ]; then
-		mv ${HOME}/.config/i3/config.original ${HOME}/.config/i3/config
-	fi
-	
-	if [ -e ${HOME}/.config/i3/i3blocks.conf.original ]; then
-		mv ${HOME}/.config/i3/i3blocks.conf.original ${HOME}/.config/i3/i3blocks.conf
-	fi
-	
-	if [ -e ${HOME}/Scripts/toggle-touchpad.sh.original ]; then
-		mv ${HOME}/Scripts/toggle-touchpad.sh.original ${HOME}/Scripts/toggle-touchpad.sh
-	fi
-	
-	if [ -e ${HOME}/Scripts/lock.sh.original ]; then
-		mv ${HOME}/Scripts/lock.sh.original ${HOME}/Scripts/lock.sh
-	fi
-	
-	if [ -e ${HOME}/Scripts/lock-icon.png.original ]; then
-		mv ${HOME}/Scripts/lock-icon.png.original ${HOME}/Scripts/lock-icon.png
-	fi
-	
-	if [ -e ${HOME}/.local/bin/jetbrains-toolbox.original ]; then
-		mv ${HOME}/.local/bin/jetbrains-toolbox.original ${HOME}/.local/bin/jetbrains-toolbox
-	fi
-	
-	if [ -e ${HOME}/.local/bin/pia.original ]; then
-		mv ${HOME}/.local/bin/pia.original ${HOME}/.local/bin/pia
+	if [ -e $linkPath.original ]; then
+		mv $linkPath.original $linkPath
 	fi
 }
 
 if ask "Restore all original files?" N; then
-	uninstall
+	unlink_file ".bashrc"
+	unlink_file ".profile"
+	unlink_file ".config/i3/config"
+	unlink_file ".config/i3/i3blocks.conf"
+	unlink_file "Scripts/toggle-touchpad.sh"
+	unlink_file "Scripts/lock.sh"
+	unlink_file "Scripts/lock-icon.png"
+	unlink_file ".local/bin/jetbrains-toolbox" "$HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox"
+	unlink_file ".local/bin/pia" "/opt/pia/run.sh"
 fi
