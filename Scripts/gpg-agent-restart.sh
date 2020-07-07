f2b7d0423d6efd3b31911fb77d53e2ce7bfcc7f4
Matthew Stiles <mstiles92@gmail.com>
2020-07-06 23:27:42 -0400

    Allow switching between multiple Yubikeys with the gpg restart script

--- Scripts/gpg-agent-restart.sh
+++ Scripts/gpg-agent-restart.sh
@@ -4,4 +4,6 @@ pkill ssh-agent
 pkill gpg-agent
 while pgrep ssh-agent > /dev/null 2>&1; do sleep 0.1; done
 while pgrep gpg-agent > /dev/null 2>&1; do sleep 0.1; done
-eval $(gpg-agent --daemon --enable-ssh-support --log-file ~/.gnupg/gpg-agent.log)
+rm -r ~/.gnupg/private-keys-v1.d
+eval $(gpg-agent --daemon --enable-ssh-support --log-file ~/.gnupg/gpg-agent.log 2>/dev/null)
+gpg --card-status > /dev/null
