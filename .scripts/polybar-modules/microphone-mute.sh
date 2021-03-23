d9a748ef3a71fc7d1bc8e3a7a8e8cf67a9aff2b9
Matthew Stiles <mstiles92@gmail.com>
2021-03-23 18:00:43 -0400

    Fix microphone mute polybar script

--- .scripts/polybar-modules/microphone-mute.sh
+++ .scripts/polybar-modules/microphone-mute.sh
@@ -3,13 +3,15 @@
 if [ -z "$1" ]; then
     mute=$(pacmd list-sources | grep "\* index:" -A 11 | grep "muted")
 else
-    mute=$(pacmd list-sources | grep "$1" -A 11 | grep "muted" )
+    mute=$(pacmd list-sources | grep "$1" -A 11 | grep "muted")
 fi
+
+# echo $mute
 
 if [ -z "$mute" ]; then
     echo "%{F#444}%{F-}"
 else
-    if [ "$mute" == *"yes"* ]; then
+    if [ -z "${mute##*yes}" ]; then
         echo "%{F#f00}%{F-}"
     else
         echo "%{F#0f0}%{F-}"
