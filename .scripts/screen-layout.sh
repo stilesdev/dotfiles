de54efeadc90469fb6776b9f0e9888526c5071bb
Matthew Stiles <mstiles92@gmail.com>
2021-06-18 18:03:10 -0400

    Use autorandr to manage display layouts

--- .scripts/screen-layout.sh
+++ .scripts/screen-layout.sh
@@ -5,14 +5,15 @@ if [ $(xrandr | grep " connected" | wc -l) -eq 1 ]; then
     echo "Auto"
     xrandr --auto
 else
-    if (xrandr | grep "DisplayPort-0" -A1 | grep "2560x1080" >> /dev/null) && (xrandr | grep "DisplayPort-1" | grep "1920x1200" >> /dev/null); then
+    if (xrandr | grep "DisplayPort-0" -A1 | grep "2560x1080" >> /dev/null) && (xrandr | grep "DisplayPort-1" -A1 | grep "1920x1200" >> /dev/null); then
         echo "Home Office"
-        while (xrandr | grep "panning" >> /dev/null); do
-            # Make sure panning is fully disabled
-            xrandr --output DisplayPort-0 --auto --panning 0x0+0+0/0x0+0+0/0/0/0/0 --output DisplayPort-1 --auto --panning 0x0+0+0/0x0+0+0/0/0/0/0
-            sleep 3
-        done
-        xrandr --output DisplayPort-0 --primary --mode 2560x1080 --rate 85 --pos 1200x460 --rotate normal --output DisplayPort-1 --mode 1920x1200 --pos 0x0 --rotate left --output DisplayPort-2 --off --output DisplayPort-3 --off
+        autorandr office-default
+#        while (xrandr | grep "panning" >> /dev/null); do
+#            # Make sure panning is fully disabled
+#            xrandr --output DisplayPort-0 --auto --panning 0x0+0+0/0x0+0+0/0/0/0/0 --output DisplayPort-1 --auto --panning 0x0+0+0/0x0+0+0/0/0/0/0
+#            sleep 3
+#        done
+#        xrandr --output DisplayPort-0 --primary --mode 2560x1080 --rate 85 --pos 1200x460 --rotate normal --output DisplayPort-1 --mode 1920x1200 --pos 0x0 --rotate left --output DisplayPort-2 --off --output DisplayPort-3 --off
     elif xrandr | grep "DP-0" | grep "3440x1440" >> /dev/null && xrandr | grep "HDMI-0" | grep "3440x1440" >> /dev/null; then
         # Ultrawide monitor picture-by-picture
         echo "Ultrawide PBP"
