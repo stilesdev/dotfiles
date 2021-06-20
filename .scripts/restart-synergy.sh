5f8f01919ee8178e4d181da28d4b55f0e140bb73
Matthew Stiles <mstiles92@gmail.com>
2021-06-20 01:21:55 -0400

    Autorandr profiles for laptop, scripts to be used when switching profiles

---
+++ .scripts/restart-synergy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+pkill synergy
+while pgrep synergy > /dev/null 2>&1; do sleep 0.1; done
+
+case $(hostname) in
+arcade* | bismuth*)
+    if [ "$AUTORANDR_CURRENT_PROFILE" == "laptop-default" ]; then
+        HOST="10.0.1.5"
+    else
+        HOST="arena.stiles.me"
+    fi
+
+    if [ $(whoami) == 'matthew' ]; then
+        synergyc --enable-crypto "$HOST"
+    else
+        su -c "synergyc --enable-crypto $HOST" - matthew
+    fi
+    ;;
+esac
