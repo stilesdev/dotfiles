e574e3c3ebb06fc5a59f9f8777d87a96e0ad8219
Matthew Stiles <mstiles92@gmail.com>
2024-01-01 18:02:22 -0500

    feat: configure zsh

--- .config/shell/cli-config.zsh
+++ .config/shell/cli-config.zsh
@@ -1,19 +1,42 @@
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
+# Configure history
+HISTFILE=~/.zhistory
 HISTSIZE=10000
 SAVEHIST=10000
-setopt appendhistory notify
-unsetopt autocd beep nomatch
+
+# Set up completions
+autoload -Uz compinit colors zcalc
+compinit -d
+colors
+
+# Speed up completions
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
+
+# Colored completion (different colors for dirs/files/etc)
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+# automatically find new executables in path
+zstyle ':completion:*' rehash true
+
+# Highlight menu selection
+zstyle ':completion:*' menu select
+
+# Use vi mode
 bindkey -v
-# End of lines configured by zsh-newuser-install
 
-# The following lines were added by compinstall
-zstyle :compinstall filename '/home/matthew/.zshrc'
+# Append session history list to history file rather than replace it, and do so incrementally instead of waiting until the shell exits
+setopt inc_append_history
 
-autoload -Uz compinit
-compinit
-# End of lines added by compinstall
+# Do not enter command lines into the history list if they are duplicates of the previous event
+setopt hist_ignore_dups
 
+# If numeric filenames are matched by a filename generation pattern, sort the filenames numerically rather than lexicographically
+setopt numeric_glob_sort
+
+# Report the status of background jobs immediately, rather than waiting until just before printing a prompt
+setopt notify
+
+unsetopt autocd beep nomatch
 
 bindkey "^H" backward-delete-word    # Ctrl+Backspace
 bindkey "^[[3~" delete-char          # Del
@@ -25,5 +48,3 @@ bindkey "^[[6~" end-of-history       # PgDown
 bindkey "^[[1;5C" forward-word       # Ctrl+Right
 bindkey "^[[1;5D" backward-word      # Ctrl+Left
 
-# Skip over duplicate history entries when using up arrow to access previous commands
-setopt histignoredups
