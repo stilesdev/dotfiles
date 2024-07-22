4344f4748cbf8f86747d940df18439557977543e
Matthew Stiles <mstiles92@gmail.com>
2024-07-22 12:18:34 -0400

    fix: unreadable executable directory colors in ls

--- .config/shell/cli-aliases.sh
+++ .config/shell/cli-aliases.sh
@@ -1,6 +1,6 @@
 # File and Dir colors for ls and other outputs
 export LS_OPTIONS='--color=auto'
-eval "$(dircolors -b)"
+eval "$(dircolors ~/.config/shell/.dircolors)"
 alias ls='ls $LS_OPTIONS'
 
 alias ll='ls -lAh'
