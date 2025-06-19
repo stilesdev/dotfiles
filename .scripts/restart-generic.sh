5844acb1cc2915501832df3f9b29e12ce9741e61
Matthew Stiles <mstiles92@gmail.com>
2025-06-18 21:58:21 -0400

    chore: remove obsolete scripts

--- .scripts/restart-generic.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-PROCESS="$1"
-
-pkill "$PROCESS"
-sleep 1
-while pgrep "$PROCESS" >/dev/null 2>&1; do sleep 0.1; done
-"$PROCESS"
--- .scripts/restart-gpg-agent.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pkill ssh-agent
-pkill gpg-agent
-while pgrep ssh-agent > /dev/null 2>&1; do sleep 0.1; done
-while pgrep gpg-agent > /dev/null 2>&1; do sleep 0.1; done
-rm -r ~/.gnupg/private-keys-v1.d
-eval $(gpg-agent --daemon --enable-ssh-support --log-file ~/.gnupg/gpg-agent.log 2>/dev/null)
-gpg --card-status > /dev/null
