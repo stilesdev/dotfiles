f1f180086a50ac93ac6be6d0cd2140345b010920
Matthew Stiles <mstiles92@gmail.com>
2026-05-19 22:33:17 -0400

    feat: update waybar config and build_version_tracker script

--- .config/waybar/custom-modules/build-version-tracker.sh
+++ .config/waybar/custom-modules/build-version-tracker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 FILE="$HOME/.config/waybar/custom-modules/build-version-tracker.txt"
+DEFAULT_VALUE="R0 0.0.0"
 
 if [ ! -e "$FILE" ]; then
-    echo -n "0.0.0" > "$FILE"
+    echo -n "$DEFAULT_VALUE" > "$FILE"
 fi
 
 VERSION=$(cat $FILE)
@@ -16,25 +17,34 @@ fi
 increment() {
     local amount=$1
 
-    local major=$(cat "$FILE" | cut -d. -f1)
-    local minor=$(cat "$FILE" | cut -d. -f2)
-    local patch=$(cat "$FILE" | cut -d. -f3)
+    local release=$(cat "$FILE" | cut -d ' ' -f1)
+    local major=$(cat "$FILE" | cut -d ' ' -f2 | cut -d. -f1)
+    local minor=$(cat "$FILE" | cut -d ' ' -f2 | cut -d. -f2)
+    local patch=$(cat "$FILE" | cut -d ' ' -f2 | cut -d. -f3)
 
     patch=$(($patch + $amount))
 
-    VERSION="$major.$minor.$patch"
+    VERSION="$release $major.$minor.$patch"
 
     echo -n "$VERSION" > "$FILE"
+}
+
+is_enabled() {
+    if [[ "$VERSION" = "$DEFAULT_VALUE" ]]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
 if [[ "$1" = "inc" ]]; then
     increment 1
     echo "$VERSION"
-    kill -SIGRTMIN+1 waybar
+    pkill -SIGRTMIN+1 waybar
 elif [[ "$1" = "dec" ]]; then
     increment -1
     echo "$VERSION"
-    kill -SIGRTMIN+1 waybar
-else
+    pkill -SIGRTMIN+1 waybar
+elif is_enabled; then
     echo "$VERSION"
 fi
