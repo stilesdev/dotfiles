fea547427b97ac209a22424620cce430474f11d1
Matthew Stiles <git@stiles.dev>
2026-06-07 20:28:42 -0400

    feat: additional ls alias

--- .config/shell/cli-aliases.sh
+++ .config/shell/cli-aliases.sh
@@ -3,6 +3,7 @@ export LS_OPTIONS='--color=auto'
 eval "$(dircolors ~/.config/shell/.dircolors)"
 alias ls='ls $LS_OPTIONS'
 
+alias l='ls -lh'
 alias ll='ls -lAh'
 
 alias cp='cp -i' # Confirm before overwriting
