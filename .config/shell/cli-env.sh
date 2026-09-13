57b4f74a4eea2a78e3d57bd9696b0e460be6bd3e
Matthew Stiles <git@stiles.dev>
2026-09-13 10:57:35 -0400

    feat: run specific apps as systemd services with memory limits
    Some apps like zen browser and firefox can grow to use way too much
    memory over time, so this prevents them from using up all system memory
    and triggering the oom-killer.
    
    Spotify also seems to have a memory leak that randomly occurs and causes
    spotify to allocate all of the memory in the system and then crash when
    it isn't able to allocate more while also triggering the oom-killer.

--- .config/shell/cli-env.sh
+++ .config/shell/cli-env.sh
@@ -6,6 +6,8 @@ export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
 export PATH="$GOBIN:$PATH"
 
+export PATH="$HOME/.local/bin:$PATH"
+
 export GPG_TTY=`tty`
 export GPG_AGENT_INFO=/run/user/$(id -u)/gnupg/S.gpg-agent::
 export SSH_AUTH_SOCK=/run/user/$(id -u)/gnupg/S.gpg-agent.ssh
