26c30c504ca9ede603150dc139cc39c501caca3b
Matthew Stiles <git@stiles.dev>
2026-09-13 21:59:40 -0400

    feat: alias shutdown and reboot to hyprshutdown when in hyprland

--- .config/shell/hyprland.sh
+++ .config/shell/hyprland.sh
@@ -3,3 +3,8 @@ if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
         exec start-hyprland
     fi
 fi
+
+if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
+    alias shutdown="hyprshutdown --top-label 'Shutting down...' --post-cmd 'shutdown -h now'"
+    alias reboot="hyprshutdown --top-label 'Rebooting...' --post-cmd 'reboot'"
+fi
